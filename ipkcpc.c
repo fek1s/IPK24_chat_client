5a19858e31640bb11fa3be3e93aee23efd7813a6
Jakub Fukala <xfukal01@stud.fit.vut.cz>
2024-03-27 21:43:27 +0100

    feat: While Loop for receive message

--- ipkcpc.c
+++ ipkcpc.c
@@ -9,6 +9,26 @@
 #include "ipkcpc_utils.h"
 #include "ipkcpc.h"
 
+void *receiveAndPrintIncomingData(void *socketFD){
+    int *socketFDPtr = (int*)socketFD;
+    int socket = *socketFDPtr;
+    char buffer[1024];
+    while (1){
+        ssize_t recvAmount = recv(socket, buffer, sizeof(buffer), 0);
+        if (recvAmount == 0){
+            printf("Server disconnected\n");
+            break;
+        }
+        else if (recvAmount == -1){
+            printf("Failed to receive data\n");
+            break;
+        }
+        else {
+            printf("Received %ld bytes: %s\n", recvAmount, buffer);
+        }
+    }
+    return NULL;
+}
 
 int main(){
     // Define server address
@@ -17,7 +37,7 @@ int main(){
 
     // Loopback address for testing
     char* ip = "127.0.0.1";
-    int16_t port = 2000;
+    int16_t port = 1999;
 
     int socketFD = createTcpSocket();
     if (socketFD == -1){
@@ -25,6 +45,8 @@ int main(){
         return 1;
     }
     printf("Socket binded successfully\n");
+
+
 
 
     struct sockaddr_in serverAddress = createServerAddress(ip, port);
@@ -39,6 +61,11 @@ int main(){
     char *line = NULL;
     size_t lineSize = 0;
     printf("Enter message:\n");
+
+    //Vytvoření vlákna
+    pthread_t tid;
+    pthread_create(&tid, NULL, receiveAndPrintIncomingData, (void*)&socketFD);
+
 
     while (1){
         ssize_t charCount = getline(&line, &lineSize, stdin);
